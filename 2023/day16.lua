314bfaecd1be4fd929c3cddb97e7efa258388919
Jonas Fan <jianfan.tw@gmail.com>
2023-12-16 22:39:08 +0800

    Update day16.lua

--- 2023/day16.lua
+++ 2023/day16.lua
@@ -7,7 +7,7 @@ local delta = {
     [">"] = {  0,  1 },
 }
 
-local function move(grid, beam, direct)
+local function move(grid, history, beam, direct)
     if beam.row < 1 or beam.row > #grid then
         return {}
     elseif beam.col < 1 or beam.col > #grid[beam.row] then
@@ -18,18 +18,17 @@ local function move(grid, beam, direct)
         row     = beam.row,
         col     = beam.col,
         direct  = direct or beam.direct,
-        history = beam.history
     }
 
     local key = out.row .. "," .. out.col
 
-    out.history[key] = out.history[key] or {}
+    history[key] = history[key] or {}
 
-    if out.history[key][out.direct] then
+    if history[key][out.direct] then
         return {}
     end
 
-    out.history[key][out.direct] = true
+    history[key][out.direct] = true
 
     if grid[out.row][out.col] == [[/]] then
         if out.direct == "^" then
@@ -53,15 +52,15 @@ local function move(grid, beam, direct)
         end
     elseif grid[out.row][out.col] == [[-]] then
         if not string.find("<>", out.direct, 1, true) then
-            local left  = move(grid, out, "<")
-            local right = move(grid, out, ">")
+            local left  = move(grid, history, out, "<")
+            local right = move(grid, history, out, ">")
 
             return table.move(left, 1, #left, #right + 1, right)
         end
     elseif grid[out.row][out.col] == [[|]] then
         if not string.find("^v", out.direct, 1, true) then
-            local up   = move(grid, out, "^")
-            local down = move(grid, out, "v")
+            local up   = move(grid, history, out, "^")
+            local down = move(grid, history, out, "v")
 
             return table.move(up, 1, #up, #down + 1, down)
         end
@@ -73,14 +72,16 @@ local function move(grid, beam, direct)
     return { out }
 end
 
-local function sol(grid, beam)
-    local nodes = { beam }
+local function sol(grid, row, col, direct)
+    local beam    = { row = row, col = col, direct = direct }
+    local nodes   = { beam }
+    local history = {}
 
     while #nodes > 0 do
         local queue = {}
 
         for _, beam in ipairs(nodes) do
-            for _, next in pairs(move(grid, beam)) do
+            for _, next in pairs(move(grid, history, beam)) do
                 queue[#queue + 1] = next
             end
         end
@@ -90,7 +91,7 @@ local function sol(grid, beam)
 
     local out = 0
 
-    for _ in pairs(beam.history) do
+    for _ in pairs(history) do
         out = out + 1
     end
 
@@ -98,34 +99,20 @@ local function sol(grid, beam)
 end
 
 local function part1(grid)
-    local beam = {
-        row     = 1,
-        col     = 1,
-        direct  = ">",
-        history = {},
-    }
-
-    return sol(grid, beam)
+    return sol(grid, 1, 1, ">")
 end
 
 local function part2(grid)
     local out = 0
 
     for row = 1, #grid do
-        for col = 1, #grid[row] do
-            if row == 1 or row == #grid or col == 1 or col == #grid[row] then
-                for _, direct in ipairs({ "^", "v", "<", ">" }) do
-                    local beam = {
-                        row     = row,
-                        col     = col,
-                        direct  = direct,
-                        history = {},
-                    }
+        out = math.max(out, sol(grid, row, 1, ">"))
+        out = math.max(out, sol(grid, row, #grid[row], "<"))
+    end
 
-                    out = math.max(out, sol(grid, beam))
-                end
-            end
-        end
+    for col = 1, #grid[1] do
+        out = math.max(out, sol(grid, 1, col, "v"))
+        out = math.max(out, sol(grid, #grid, col, "^"))
     end
 
     return out
